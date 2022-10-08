ee6449732a81789137f702569eb7f92ce2bf83e3
Kyle Holmstrom <holmstrom.kyle@yahoo.com>
2022-10-08 12:18:41 -0400

    changed file structure around

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
 const app = express();
-const PORT = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
-app.use((req, res, next) => {
-    res.send('Express Sever is working..');
-});
+app.use(cors());
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+app.use(express.json());
+
+app.listen(port, () => {
+    console.log(`Server running on port: ${port}`);
 });
