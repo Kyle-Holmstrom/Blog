25e2ce6c02de62e3b1dd0de23ba5efb98d50fd56
Kyle Holmstrom <holmstrom.kyle@yahoo.com>
2022-10-10 17:34:17 -0400

    Added in another property for user schema to check if the account is an admin or not

--- backend/models/User.js
+++ backend/models/User.js
@@ -6,7 +6,8 @@ const UserSchema = new mongoose.Schema({
     email: { type: String, required: true },
     userName: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    avatar: { type: String, required: false }
+    avatar: { type: String, required: false },
+    isAdmin: { type: Number, required: true }
 });
 
 module.exports = User = mongoose.model('user', UserSchema);
