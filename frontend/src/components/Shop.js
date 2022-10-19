e149d9dea4e8b14746064c9640bd8d6ad72faad5
Kyle Holmstrom <kyle@system76-pc.localdomain>
2022-10-19 11:14:06 -0400

    Added images to the shop to get a layout started.

--- frontend/src/components/Shop.js
+++ frontend/src/components/Shop.js
@@ -7,6 +7,24 @@ const Shop = () => {
         <div className="shop-container">
             <h1>Welcome to the Shop!</h1>
             <h4>Make a purchase to support your favorite author!</h4>
+
+
+	    <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-evenly'}}>
+	    <img src={'https://www.feedinspiration.com/wp-content/uploads/2015/06/Clothing-For-Teenage-Girls-.jpg'} 
+		style={{width: '120px', height: '140'}}
+		    />
+	
+	    <img src={'https://www.theskinnybeep.com/wp-content/uploads/2016/10/Fashion-Model-Valentino-Catwalk.jpg'} 
+		style={{width: '120px', height: '140'}}
+	    />
+	
+	
+	    <img src={'https://allforfashiondesign.com/wp-content/uploads/2013/05/fa-9.jpg'} 
+		style={{width: '120px', height: '140'}} />
+	</div>
+
+
+
         </div>
     );
 }
