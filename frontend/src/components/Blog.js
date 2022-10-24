587f1a12fd7f79d7a97f95f6a084887f7bcd75c2
Kyle Holmstrom <holmstrom.kyle@yahoo.com>
2022-10-24 15:24:18 -0400

    added in some styling to the blog post containers

--- frontend/src/components/Blog.js
+++ frontend/src/components/Blog.js
@@ -1,6 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import Button from '@mui/material/Button';
 import Badge from '@mui/material/Badge';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import Card from '@mui/material/Card';
@@ -9,8 +7,9 @@ import './Blog.css';
 
 const BlogPost = (props) => (
     <Card variant="outlined" className="blogpost-container" >
-    {/* <div className="blogpost-container"> */}
-        <h1>{props.post.title}</h1>
+        <div className='blogpost-hero'>
+            <h1>{props.post.title}</h1>
+        </div>
         <article>{props.post.description}</article>
         <footer>
             <ul>
@@ -19,43 +18,17 @@ const BlogPost = (props) => (
             </ul> 
         </footer>
         <div className='comment-container'>
-            <h4>Leave a comment below!</h4>
             <section>
                 <p>{props.post.comments}</p>
             </section>
-            <textarea rows="7" cols="75" />
+            <textarea rows="5" cols="67" placeholder='Leave a comment!' />
         </div>
         <aside>
             <Badge badgeContent={props.post.upvote} >
                 <FavoriteIcon className="FavoriteIcon" />
             </Badge>
         </aside>
-    {/* </div> */}
     </Card>
-    
-    
-    // <tr>
-    //     <td>{props.post.title}</td>
-    //     <td>{props.post.description}</td>
-    //     <td>{props.post.comments}</td>
-    //     <td>{props.post.upVote = 0}</td>
-    //     <td>{props.post.creator}</td>
-	//     <td>{props.post.createdAt}</td>
-    //     <td>
-    //         <Button variant="contained" >
-    //             <Link to={`/blog-edit/${props.post._id}`}
-    //                 style={{color: 'white', textDecoration: 'none'}}
-    //                 >Edit</Link>
-    //             </Button> 
-    //         <Button variant="contained" style={{marginLeft: '5px'}}
-    //             onClick={() => {
-    //                 props.deleteBlogPost(props.post._id);
-    //             }}
-    //         >
-    //             Delete
-    //         </Button>
-    //     </td>
-    // </tr>
 );
 
 export default function Blog() {
@@ -106,29 +79,6 @@ export default function Blog() {
     }
 
     return (
-        // <div className="post-list-container">
-        //     <h3>Post List</h3>
-        //     <table>
-        //         <thead>
-        //             <tr>
-        //                 <th>Title</th>
-        //                 <th>Description</th>
-        //                 <th>Comments</th>
-        //                 <th>Upvotes</th>
-        //                 <th>Creator</th>
-	    // 		        <th>Created At</th>
-        //             </tr>
-        //         </thead>
-        //         <tbody>
-        //             {blogPostList()}
-        //         </tbody>
-        //     </table> 
-	    
-	    // <Button variant="contained" className="add-blogpost-btn">
-        //         <Link to="/blog/add" className="add-blogpost-link">Create Blog Post</Link>
-        //     </Button>
-	   
-        // </div>
         <div>{blogPostList()}</div>
     );
 }
