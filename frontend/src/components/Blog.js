6c5e33302bcef1d59d4b5d856e899b6e3aa2486c
Kyle Holmstrom <holmstrom.kyle@yahoo.com>
2023-04-28 22:10:05 -0400

    Reverting last 3 git commits

--- frontend/src/components/Blog.js
+++ frontend/src/components/Blog.js
@@ -41,8 +41,7 @@ export default function Blog() {
     // This method will fetch the blog post from the database.
     useEffect(() => {
         async function getBlogPost() {
-            const response = await fetch(`https://blog-udip.onrender.com/blog`);
-            // const response = await fetch(`http://localhost:4000/blog`);
+            const response = await fetch(`http://localhost:4000/blog`);
             // const response = await fetch("https://kyle-holmstrom.github.io/blog/");
 
             if (!response.ok) {
@@ -63,7 +62,7 @@ export default function Blog() {
     
     // this method will delete a record
     async function deleteBlogPost(id) {
-        await fetch(`https://blog-udip.onrender.com/blog/${id}`, {
+        await fetch(`http://localhost:4000/blog/${id}`, {
             method: "DELETE"
         });
         const newPost = posts.filter((el) => el._id !== id);
